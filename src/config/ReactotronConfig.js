15a33fb458c1c6947d6a3efdedf5ae231b59b4ad
brendonguedes <lordbguedes@gmail.com>
2020-01-28 17:03:24 -0300

    reactotron configurações / ip / redirect tcp:9090

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure()
+  const tron = Reactotron.configure({ host: '177.133.189.198' })
     .useReactNative()
     .connect();
 
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,5 +1,5 @@
-import styled from "styled-components/native";
-import { RectButton } from "react-native-gesture-handler";
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
@@ -14,7 +14,7 @@ export const Form = styled.View`
 `;
 
 export const Input = styled.TextInput.attrs({
-  placeholderTextColor: "#999"
+  placeholderTextColor: '#999',
 })`
   flex: 1;
   height: 40px;
