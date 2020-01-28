7ced75488fef58c8b63dfb62a97da8eb13b47354
brendonguedes <lordbguedes@gmail.com>
2020-01-28 11:49:48 -0300

    styled components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Container } from './styles';
 
 // import { Container } from './styles';
 
 export default function Main() {
-  return <View />;
+  return <Container />;
 }
 
 Main.navigationOptions = {
-  title: 'Hello world!',
+  title: 'Usuários',
 };
